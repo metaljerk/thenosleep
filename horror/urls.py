bca3c86aa85ecfed1c047f7f128ca4f0d0674da9
rpraveenpai <rpraveenpai065@gmail.com>
2020-11-12 14:01:39 +0530

    fixed view name error

--- horror/urls.py
+++ horror/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.hot_post, name="hot_post"),
     path('about', views.about, name="about"),
     path('<str:id>/', views.nosleep, name="nosleep"),
-    path('nosleep/api', views.RedditView.as_view()),
+    path('api/list', views.RedditListView.as_view()),
     path('api/read/<str:id>/', views.RedditReadView.as_view())
 ]
